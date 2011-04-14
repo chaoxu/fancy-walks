1b9f5489d349ee6ca11a73a9a4a4a9131094dca2
Bin Jin <bjin1990@gmail.com>
2011-04-14 17:19:46 +0800

    solve codeforces 75D with GHC parallel routines

--- codeforces.com/75/D.hs
+++ codeforces.com/75/D.hs
@@ -7,6 +7,26 @@ import Data.Array
 import Control.Applicative
 import qualified Data.ByteString.Char8 as BS
 
+--import Control.Parallel
+import qualified GHC.Conc (par, pseq)
+infixr 0 `par`, `pseq`
+par = GHC.Conc.par
+pseq = GHC.Conc.pseq
+
+foldMap :: Monoid m => (a -> m) -> [a] -> m
+foldMap f x = foldMap' x (length x)
+  where
+    foldMap' xs ln
+        | ln == 0    = mempty
+        | ln >= 100  = zs' `par` (ys' `pseq` (ys' `mappend` zs'))
+        | otherwise  = foldl1 mappend $ map f xs
+      where
+        lny     = ln `div` 2
+        lnz     = ln - lny
+        (ys,zs) = splitAt lny xs
+        ys'     = foldMap' ys lny
+        zs'     = foldMap' zs lnz
+
 data MaxSum a = MaxSum
               { prefixS :: a
               , middleS :: a
@@ -22,8 +42,6 @@ instance (Bounded a, Num a, Ord a) => Monoid (MaxSum a) where
         mid = middleS x `max` middleS y `max` (suffixS x + prefixS y)
         suf = suffixS y `max` (allS y + suffixS x)
         sum = allS x + allS y
-    mconcat [] = mempty
-    mconcat xs = foldl1 mappend xs
 
 singleton :: Int -> MaxSum Int64
 singleton x = MaxSum x' x' x' x'
@@ -35,8 +53,8 @@ readI = fst . fromJust . BS.readInt
 main = do
     [n, m] <- map read . words <$> getLine :: IO [Int]
     lines <- BS.lines <$> BS.getContents
-    let states = map (mconcat . map singleton . tail . map readI . BS.words) $ take n lines
+    let states = map (foldMap singleton . tail . map readI . BS.words) $ take n lines
     let list = map readI . take m . BS.words . head $ drop n lines
     let arr = listArray (1, n) states :: Array Int (MaxSum Int64)
-    let answer = mconcat $ map (arr!) list
+    let answer = foldMap (arr!) list
     print $ middleS answer
