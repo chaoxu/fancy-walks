02df9c0752a7576f108b57ed0a91a1d4ad2e83a4
Bin Jin <bjin1990@gmail.com>
2011-04-24 00:46:18 +0800

    add pandoc lhs support

--- .clean.sh
+++ .clean.sh
@@ -8,6 +8,6 @@ done
 
 echo "cleaning object files generated during compile time"
 
-for file in `find . -type f -path ./[0-9a-Az-Z]\* \( -name \*.hi -or -name \*.o \) ` ; do
+for file in `find . -type f -path ./[0-9a-Az-Z]\* \( -name \*.hi -or -name \*.o -or -name \*.aux -or -name \*.log -or -name \*.out \) ` ; do
     rm $file -i
 done
