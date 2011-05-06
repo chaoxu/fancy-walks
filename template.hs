40965c60546e2d65dcead0d1d23c222d44be7012
Bin Jin <bjin1990@gmail.com>
2011-05-07 01:54:26 +0800

    add haskell template

---
+++ template.hs
@@ -0,0 +1,38 @@
+{-# OPTIONS_GHC -O2 #-}
+
+import Data.List
+import Data.Maybe
+import Data.Char
+import Data.Array
+import Data.Int
+import Data.Ratio
+import Data.Bits
+import Data.Function
+import Data.Ord
+import Control.Monad.State
+import Control.Monad
+import Control.Applicative
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Data.Tree
+import Data.Graph
+
+parseInput = do 
+    n <- readInt
+    s <- readString
+    return (n, s)
+  where
+    readInt = state $ fromJust . BS.readInt . BS.dropWhile isSpace
+    readString = state $ BS.span (not . isSpace) . BS.dropWhile isSpace
+
+main = do
+    (n, s) <- evalState parseInput <$> BS.getContents
+    print $ replicate n s
