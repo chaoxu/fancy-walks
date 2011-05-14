0fb8e892d5665d4b6bdacb21f30eca4ff5ee9b4d
Bin Jin <bjin1990@gmail.com>
2011-05-14 16:26:58 +0800

    more aggressive clean script

--- .clean.sh
+++ .clean.sh
@@ -2,12 +2,8 @@
 
 echo "cleaning executable files"
 
-for file in `find . -executable -type f -path ./[0-9a-Az-Z]\*` ; do
-    rm $file -i
-done
+find . -executable -type f -path ./[0-9a-Az-Z]\* -delete
 
 echo "cleaning object files generated during compile time"
 
-for file in `find . -type f -path ./[0-9a-Az-Z]\* \( -name \*.hi -or -name \*.o -or -name \*.aux -or -name \*.log -or -name \*.out \) ` ; do
-    rm $file -i
-done
+find . -type f -path ./[0-9a-Az-Z]\* \( -name \*.hi -or -name \*.o -or -name \*.aux -or -name \*.log -or -name \*.out \) -delete
