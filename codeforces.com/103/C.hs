6c0d4c61b306df2dc8750698ccb7433fa3acff28
Bin Jin <bjin1990@gmail.com>
2011-08-07 23:32:03 +0800

    codeforces: 103 ABC

---
+++ codeforces.com/103/C.hs
@@ -0,0 +1,122 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+{-# OPTIONS_GHC -O2 #-}
+
+import Data.List
+import Data.Maybe
+import Data.Char
+import Data.Array.IArray
+import Data.Array.Unboxed (UArray)
+import Data.Int
+import Data.Ratio
+import Data.Bits
+import Data.Function
+import Data.Ord
+--import Control.Monad.State
+import Control.Monad
+import Control.Applicative
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.Sequence (Seq, (<|), (|>), (><), ViewL(..), ViewR(..))
+import qualified Data.Sequence as Seq
+import qualified Data.Foldable as F
+import Data.Graph
+
+import Debug.Trace
+
+parseInput = do 
+    n <- readInteger
+    k <- readInteger
+    p <- readInt
+    query <- replicateM p readInteger
+    return (n, k, query)
+  where
+    readInt = state $ fromJust . BS.readInt . BS.dropWhile isSpace
+    readInteger = state $ fromJust . BS.readInteger . BS.dropWhile isSpace
+    readString = state $ BS.span (not . isSpace) . BS.dropWhile isSpace
+    readLine = state $ BS.span (not . isEoln) . BS.dropWhile isEoln
+    isEoln ch = ch == '\r' || ch == '\n'
+
+main = putStr =<< solve . evalState parseInput <$> BS.getContents
+
+solve (n, k, query) = map solvePosition query
+  where
+    solvePosition x
+        | k == 0    = '.'
+        | k == n    = 'X'
+        | k == 1    = if x == n then 'X' else '.'
+        | partLeft  = if x == left then 'X' else '.'
+        | partA     = if (x - left) `mod` 2 == 0 then 'X' else '.'
+        | otherwise = 'X'
+      where
+        partLeft = x <= left
+        partA = (x - left) <= 2 * a
+
+    minB = n `mod` 2
+
+    -- 2 * a + b <= n - 2
+    -- a + b + 1 == k
+
+    a = ((n - 2) - (k - 1)) `min` (k - 1 - minB)
+    b = k - 1 - a
+
+    left = n - 2 * a - b
+
+
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+
+class (Monad m) => MonadState s m | m -> s where
+	get :: m s
+	put :: s -> m ()
+
+modify :: (MonadState s m) => (s -> s) -> m ()
+modify f = do
+	s <- get
+	put (f s)
+
+gets :: (MonadState s m) => (s -> a) -> m a
+gets f = do
+	s <- get
+	return (f s)
+
+newtype State s a = State { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+	fmap f m = State $ \s -> let
+		(a, s') = runState m s
+		in (f a, s')
+
+instance Applicative (State s) where
+    pure = return
+    (<*>) = ap
+
+instance Monad (State s) where
+	return a = State $ \s -> (a, s)
+	m >>= k  = State $ \s -> let
+		(a, s') = runState m s
+		in runState (k a) s'
+
+instance MonadState s (State s) where
+	get   = State $ \s -> (s, s)
+	put s = State $ \_ -> ((), s)
+
+evalState :: State s a -> s -> a
+evalState m s = fst (runState m s)
+
+execState :: State s a -> s -> s
+execState m s = snd (runState m s)
+
+mapState :: ((a, s) -> (b, s)) -> State s a -> State s b
+mapState f m = State $ f . runState m
+
+withState :: (s -> s) -> State s a -> State s a
+withState f m = State $ runState m . f
+
+state = State
